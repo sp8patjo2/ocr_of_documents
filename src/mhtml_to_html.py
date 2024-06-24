3feac20895280b2f84081b6632535d826b640501
sp8patjo2 <sp8patrik@gmail.com>
2024-06-24 15:38:42 +0200

    test, change main() - add defaults for debugging

--- src/mhtml_to_html.py
+++ src/mhtml_to_html.py
@@ -4,15 +4,16 @@ from classes.mhtml_to_text_and_images import MHTMLToTextAndImages
 
 def main():
     parser = argparse.ArgumentParser(description='Convert MHTML to HTML and JPEG.')
-    parser.add_argument('mhtml_file', help='Path to the MHTML file.')
-    parser.add_argument('output_folder', help='Path to the output folder.')
+    #parser.add_argument('mhtml_file', help='Path to the MHTML file.')
+    parser.add_argument('--file', type=str, default='../exempel/handheld/Instruktion återställning av Wi-Fi, mobildata och Bluetooth på modell EDA52.mhtml', help='Path to the MHTML file.')
+    parser.add_argument('--output', default="removeme_test", help='Path to the output folder.')
     args = parser.parse_args()
 
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger(__name__)
 
     converter = MHTMLToTextAndImages(logger)
-    html_file, resources = converter.convert(args.mhtml_file, args.output_folder)
+    html_file, resources = converter.convert(args.file, args.output)
     logger.info(f'HTML file saved to {html_file}')
     logger.info(f'Resources saved: {resources}')
 
