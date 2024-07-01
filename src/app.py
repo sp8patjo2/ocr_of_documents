0bcd01fd3801b2a775658b7080bef4d1c47322b6
sp8patjo2 <sp8patrik@gmail.com>
2024-07-01 15:53:36 +0200

    lägger till stöd för att processa båda typerna av filer, inte testat än

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ from classes.mhtml_to_text_and_images import MHTMLToTextAndImages
 logger = None
 
 def setup_logger(log_level: int) -> logging.Logger:
-    logger = logging.getLogger("pdf_to_html_converter")
+    logger = logging.getLogger("file_to_html_converter")
     logger.setLevel(log_level)
     handler = logging.StreamHandler()
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
@@ -32,6 +32,19 @@ def pdf_to_html_jpeg(pdf_path, output_folder):
     return output_html_path
 
 
+def mhtml_to_html_jpeg(mhtml_path, output_folder):
+    global logger
+    converter = MHTMLToTextAndImages(logger)
+
+    logger.info(f"Starting MHTML to HTML conversion of {mhtml_path} in folder: {os.getcwd()}")
+    pages_elements = converter.extract_images_and_text(mhtml_path, output_folder)
+    output_html_path = os.path.join(output_folder, f"{output_folder}.html")
+
+    converter.generate_html(pages_elements, output_html_path)
+    logger.info(f"HTML file created at: {output_html_path}")
+    return output_html_path
+
+
 def jpeg_to_markdown(jpeg_directory):
     global logger
     processor = AIImageProcessor()
@@ -46,45 +59,40 @@ def jpeg_to_markdown(jpeg_directory):
                 logger.info(f"AI-analyzing {source_image} - result is in {output_file}")
 
 
-def process_pdf_file(pdf_path):
-    output_folder = os.path.splitext(os.path.basename(pdf_path))[0]
+def process_file(file_path):
+    output_folder = os.path.splitext(os.path.basename(file_path))[0]
 
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    pdf_to_html_jpeg(pdf_path, output_folder)
+    if file_path.lower().endswith('.pdf'):
+        pdf_to_html_jpeg(file_path, output_folder)
+    elif file_path.lower().endswith('.mhtml') or file_path.lower().endswith('.mht'):
+        mhtml_to_html_jpeg(file_path, output_folder)
+    else:
+        return False
+
     jpeg_to_markdown(output_folder)
+    return True
 
 
-
-def process_pdf_directory(path):
+def process_directory(path):
     unprocessed_files = []
-    pdf_pattern = re.compile(r'^.*\.pdf$', re.IGNORECASE)
-    
     for file in os.listdir(path):
-        pdf_path = os.path.join(path, file)
+        file_path = os.path.join(path, file)
 
-        if not os.path.isfile(pdf_path):
+        if not os.path.isfile(file_path):
             continue  # do not collect subfolders in unprocessed ... it's expected. NEXT
         
-        if not pdf_pattern.match(file):
-            unprocessed_files.append(pdf_path)  # does not match PDF ... save to be able to inform user (last)
-            continue
+        if not process_file(file_path):
+            unprocessed_files.append(file_path)  # save to be able to inform user (last)
 
-        # the actual processing
-        process_pdf_file(pdf_path)
-    
-    
-    # Done ... any messages to the end user?
     if not unprocessed_files:
         return
     
     logger.info("The following files were not processed:")
     for file in unprocessed_files:
         logger.info(file)
-    
-
-
 
 
 def main() -> None:
@@ -97,31 +105,27 @@ def main() -> None:
     logger = setup_logger(log_level)
 
     # parse arguments, there is a default file set
-    parser = argparse.ArgumentParser(description="Convert PDF to HTML with extracted images.")
+    parser = argparse.ArgumentParser(description="Convert PDF or MHTML to HTML with extracted images.")
     parser.add_argument("--pdf", help="Path to the PDF file.")
-    parser.add_argument("--path", help="Path to the directory containing PDF files.")
+    parser.add_argument("--path", help="Path to the directory containing PDF or MHTML files.")
     args = parser.parse_args()
 
     logger.info(f"Current working dir is: {os.getcwd()}")
 
-    args.path = 'Handdatorer'
-
     if args.pdf and args.path:
         raise ValueError("Both --pdf and --path cannot be specified at the same time.")
     
-    elif args.path:
-        debug = f"{os.path.join(os.getcwd(), args.path)}"
-        logger.info(f"processning {debug}")
-        process_pdf_directory(args.path)
-                
-    elif args.pdf:
-        process_pdf_file(args.pdf)
-        
-        
-    else:
+    if not args.pdf and not args.path:
         logger.error("Either --pdf or --path must be specified.")
+        return
 
+    if args.path:
+        debug = f"{os.path.join(os.getcwd(), args.path)}"
+        logger.info(f"Processing {debug}")
+        process_directory(args.path)
+        return
 
+    process_file(args.pdf)
 
 
 if __name__ == "__main__":
